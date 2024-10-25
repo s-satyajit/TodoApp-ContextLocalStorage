7284c86586c8167aab60623be493e524d72c92a0
Satyajit Samal <samalsatyajit668@gmail.com>
2024-10-25 08:48:32 +0530

    Added all the functionalities and with the context in todoform

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,18 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
               {/* Todo form goes here */} 
+              <TodoItem />
             </div>
             <div className="flex flex-wrap gap-y-3">
               {/*Loop and Add TodoItem here */}
+              {todos.map((todo) => (
+                <div 
+                key={todo.id}
+                className="w-full"
+                >
+                  <TodoForm />
+                </div>
+              ))}
             </div>
         </div>
         </div>
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,6 +12,7 @@ function TodoForm() {
 
         if(!todo) return 
         addTodo({todo, completed: false})
+        setTodo('')
     }
 
 
